b898c3435b1f6729709d8604481e22b8bb98c060
Felipe Abreu <felipe.abreu654@gmail.com>
2025-11-26 12:14:22 -0300

    theme definition and configuration

---
+++ src/styles/theme/fonts.js
@@ -0,0 +1,21 @@
+const fonts = {
+  lineHeights: {
+    normal: "130%",
+    medium: "150%",
+  },
+  sizes: {
+    xs: "0.75rem",
+    sm: "0.85rem",
+    base: "1rem",
+    md: "1.25rem",
+    lg: "2.25rem",
+    xl: "3.5rem",
+  },
+  weights: {
+    regular: 400,
+    semibold: 600,
+    bold: 600,
+  },
+};
+
+export default fonts;
