d2c28e148a30085188487df8304e44023a285236
Felipe Abreu <felipe.abreu654@gmail.com>
2025-11-27 10:53:34 -0300

    remove comments

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,8 +1,3 @@
-// Posts
-// ● GET https://tech-test-backend.dwsbrazil.io/posts/: Get information about all posts.
-// ● GET https://tech-test-backend.dwsbrazil.io/posts/{ID}: Get information about a
-// specific post.
-
 import ENDPOINTS from "./constants";
 
 export const getPosts = () =>
